4dd0a6509da5ec7e7f472e333ccb946bc51e5ceb
iacobcl <iacob.claudia@gmail.com>
2024-03-04 21:50:08 +0000

    Add files via upload

---
+++ test/mathmanager_test.dart
@@ -0,0 +1,17 @@
+import 'package:test/test.dart';
+import 'package:MathManagerDart-main/lib/mathmanager.dart';
+
+void main() {
+  group('Test all methods of mathmanager', () {
+    test('Test add 2 positive integers', () {
+	mathmanager math = mathmanager();
+        expect(math.add(0, 3), equals(3));
+    });    
+    test('Test add 2 negative integers', () {
+	mathmanager math = mathmanager();
+      	expect(math.add(-1, -3), equals(-4));
+    });  
+});
+}
+
+    
