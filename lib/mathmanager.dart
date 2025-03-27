dd007e0a41009e415af0c36ae7c597a77d16e1c4
Jodie <jodiewlfan@gmail.com>
2025-03-27 11:11:18 +0000

    idk how tax works, but some tests done

--- lib/mathmanager.dart
+++ lib/mathmanager.dart
@@ -1,14 +1,37 @@
-class mathmanager{
+class mathmanager {
+  int add(int a, int b) {
+    return a + b;
+  }
 
-	int add(int a, int b){
-			return a+b;
-	}
+  int subtract(int a, int b) {
+    return a - b;
+  }
 
-	int subtract(int a, int b){
-			return a-b;
-	}
+  int multiply(int a, int b) {
+    return a * b;
+  }
 
-	int multiply(int a, int b){
-			return a*b;
-	}
+  double interest(double amount, int year) {
+    if (amount <= 0) {
+      return -1;
+    } else if (year == 1) {
+      return amount * .038 / 12;
+    } else if (year == 2) {
+      return amount * .036 / 12;
+    } else {
+      return -2; // result is year is -1, 0, 3 etc.
+    }
+  }
+
+  double taxPaid(double income) {
+    if (income >= 0 && income <= 12570) {
+      return 0;
+    } else if (income <= 50270) {
+      return .2 * (income - 12570);
+    } else if (income <= 125140) {
+      return .4 * (income); // wrong
+    } else {
+      return -1;
+    }
+  }
 }
--- test/mathmanager_test.dart
+++ test/mathmanager_test.dart
@@ -4,14 +4,24 @@ import '../lib/mathmanager.dart';
 void main() {
   group('Test all methods of mathmanager', () {
     test('Test add 2 positive integers', () {
-	mathmanager math = mathmanager();
-        expect(math.add(0, 3), equals(3));
-    });    
+      mathmanager math = mathmanager();
+      expect(math.add(0, 3), equals(3));
+    });
     test('Test add 2 negative integers', () {
-	mathmanager math = mathmanager();
-      	expect(math.add(-1, -3), equals(-4));
-    });  
-});
+      mathmanager math = mathmanager();
+      expect(math.add(-1, -3), equals(-4));
+    });
+    test('Calculate monthly interest', () {
+      mathmanager math = mathmanager();
+      expect(math.interest(1000, 2), equals(3.0));
+    });
+    test('Calculate monthly interest2', () {
+      mathmanager math = mathmanager();
+      expect(math.interest(7000, 3), equals(-2));
+    });
+    test('Calculate monthly interest3', () {
+      mathmanager math = mathmanager();
+      expect(math.interest(-1, 2), equals(-1));
+    });
+  });
 }
-
-    
